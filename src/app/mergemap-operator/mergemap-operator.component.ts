4214620ff04734f3efab20c09e4f8bbae49a4a7c
heetpatel7848 <heet.sabhaya@bacancy.com>
2023-06-13 11:39:05 +0530

    created merge map operator adn utility service

--- src/app/mergemap-operator/mergemap-operator.component.ts
+++ src/app/mergemap-operator/mergemap-operator.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Subscription, from, map, mergeAll, mergeMap, of } from 'rxjs';
 import { OperatorService } from '../service/operator.service';
+import { DesignUtilityService } from '../service/design-utility.service';
 
 @Component({
   selector: 'app-mergemap-operator',
   templateUrl: './mergemap-operator.component.html',
   styleUrls: ['./mergemap-operator.component.scss']
 })
-export class MergemapOperatorComponent {
+export class MergemapOperatorComponent implements OnInit {
+
+
+  getData(data: string) {
+    return of(data + ' video uploaded');
+  }
+
+  constructor(private du: DesignUtilityService) { }
+
+  ngOnInit(): void {
+    const source = from(['Tech', 'Comedy', 'News']);
+
+    source.pipe(
+      mergeMap(res => this.getData(res),
+
+      )
+    ).subscribe(res => {
+      console.log(res);
+      this.du.print(res, 'elContainer3')
+    })
+
+
+  }
+
+
 
 }
+
+
